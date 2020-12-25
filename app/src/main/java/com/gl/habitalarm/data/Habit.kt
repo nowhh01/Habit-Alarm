2e99f14288536fd626aca4a721517cfad400a1b2
nowhh01 <39201456+nowhh01@users.noreply.github.com>
2020-12-25 17:07:36 -0500

    remove attributes

--- app/src/main/java/com/gl/habitalarm/data/Habit.kt
+++ app/src/main/java/com/gl/habitalarm/data/Habit.kt
@@ -1,6 +1,5 @@
 package com.gl.habitalarm.data
 
-import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -11,9 +10,7 @@ data class Habit(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "habit_id")
     val id: Long,
-    @NonNull
     val name: String,
     val days: BooleanArray = BooleanArray(7),
-    val notifyingTime: LocalTime
-    ) {
-}
+    val notifyingTime: LocalTime?
+)
--- app/src/main/java/com/gl/habitalarm/data/Repetition.kt
+++ app/src/main/java/com/gl/habitalarm/data/Repetition.kt
@@ -1,6 +1,5 @@
 package com.gl.habitalarm.data
 
-import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -11,11 +10,8 @@ data class Repetition(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "repetition_id")
     val id: Long,
-    @NonNull
     @ColumnInfo(name = "repetition_habit_id")
     val habitId: Long,
-    @NonNull
-    val data: LocalDate,
+    val date: LocalDate,
     val state: Int
-) {
-}
+)
